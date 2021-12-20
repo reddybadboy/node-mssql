f5adabb5a185c448cacd2de9b193947cd801c338
reddybadboy <85503733+reddybadboy@users.noreply.github.com>
2021-12-20 11:59:18 +0530

    Update liquibase.sh

--- liquibase.sh
+++ liquibase.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 set -x
 cd sqljdbc_9.4/enu
-liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://EC2AMAZ-VC6EOHH:1433;databaseName=testdb;integratedSecurity=false;" --changeLogFile="changset.sql"  --username=testuser  --password=admin1234 update;
+liquibase --driver=com.microsoft.sqlserver.jdbc.SQLServerDriver --classpath="/home/ubuntu/sqljdbc_9.4/enu/mssql-jdbc-9.4.0.jre8.jar" --url="jdbc:sqlserver://:1433;databaseName=testdb1;integratedSecurity=false;" --changeLogFile="changset.sql"  --username=testuser  --password=admin1234 update;
 sudo rm -rf sqljdbc_9.4/enu/changset.sql
